16939c594c35d2dfaffd1c496bebbfa44725b67c
dEmilonte <brigade404@gmail.com>
2026-01-30 19:54:27 +0500

    Small Optimization
    Used more optimal code for maxHeight

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -30,7 +30,7 @@ namespace Model.Runtime.Projectiles
             // Insert you code here
             ///////////////////////////////////////
 
-            float maxHeight = (totalDistance / 10) * 6;
+            float maxHeight = totalDistance * 0.6f;
             localHeight = maxHeight * (-(t * 2 - 1) * (t * 2 - 1) + 1);
 
 
